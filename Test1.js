273c0658f0118a41ebb4d460d818979d487d81f5
Maruska88 <marija.marus@gmail.com>
2026-01-20 22:47:13 +0100

    commit

--- Test1.js
+++ Test1.js
@@ -1,25 +1,12 @@
-/// <reference types="Cypress" />
+// cypress.config.js - Create in C:\Users\Marija\Documents\GitHub\Algebra-FE\
+const { defineConfig } = require("cypress");
 
-describe('Contact us', () => {
-    it('Get in touch', () => {
-
-cy.visit("https://wearenotch.com/contact/");
-cy.wait(2000)
-
-cy.get('#firstname').type('abc')
-
-cy.get('#email').type('example@example.com')
-
-cy.get('#lastname').type('cba')
-
-cy.wait(1000)
-
-cy.get(':nth-child(6) > .e-checkboxButton__label').click(//label[normalize-space()='Quality Assurance']),
-
-cy.get('.color-space-gray').click(//label[for='pp']),
-
-cy.get('.e-btn')
-.should('be.visible')
-
-    ))
-    })})
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:3000", // Your app URL
+    supportFile: "cypress/support/e2e.js",
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    screenshotsFolder: "cypress/screenshots",
+    videosFolder: "cypress/videos",
+  },
+});
